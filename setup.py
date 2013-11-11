377301372bb75b2769ee7a8c1a147c7885090317
Jacob Haslehurst <jacob@haslehurst.net>
2013-11-11 22:05:21 +1100

    fixing typo in setup.py description field

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='drf_ujson',
     version='1.1',
-    description='Dkango Rest Framework UJSON Renderer',
+    description='Django Rest Framework UJSON Renderer',
     author='Gizmag',
     author_email='tech@gizmag.com',
     url='https://github.com/gizmag/drf-ujson-renderer',
