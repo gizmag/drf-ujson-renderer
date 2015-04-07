8d076e6c9b74f814bcac1618c873e911f783b291
Jacob Haslehurst <jacob@haslehurst.net>
2015-04-07 16:27:22 +1000

    bump version to v1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='drf_ujson',
-    version='1.1',
+    version='1.2',
     description='Django Rest Framework UJSON Renderer',
     author='Gizmag',
     author_email='tech@gizmag.com',
